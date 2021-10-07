4cd2693913678f8bbe615cd1872ba42ba4b60579
ivandi1980 <ivandi.djoh@gmail.com>
2021-10-07 20:54:30 +0800

    added jetstream

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+// Route::get('/', function() {
+//     $title = "FlatsApp";
+
+
+//     return view('home', compact('title'));
 // });
 
 
-Route::get('/', function() {
-    $title = "FlatsApp";
-
-
-    return view('home', compact('title'));
-});
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');

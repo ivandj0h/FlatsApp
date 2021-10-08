4b27b8f6c696639c74bac12bce6b628bb51a95a7
ivandi1980 <ivandi.djoh@gmail.com>
2021-10-08 13:50:02 +0800

    readme updated!

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,15 @@
 @extends('layouts.main')
-@section('container')
-    <h3>Home</h3>
-@endsection
+    @section('container')
+        <div class="col-lg-12 text-center mt-5">
+            <h1>{{ $title }}</h1>
+            <h4>Find Flats here!</h4>
+        </div>
+        <div class="col-md-4 offset-md-4 mt-1 pt-3">
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Search ......" aria-label="Recipient's username">
+            {{-- <div class="input-group-append">
+            <span class="input-group-text"><i class="fa fa-search"></i></span>
+            </div> --}}
+            </div>
+        </div>
+    @endsection

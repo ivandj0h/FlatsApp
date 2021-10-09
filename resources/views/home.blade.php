5c4e36316d8d45916dbab75907c55eefd74ff051
ivandi1980 <ivandi.djoh@gmail.com>
2021-10-09 17:11:59 +0800

    search created

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,19 +5,28 @@
             <h4>Find Flats here!</h4>
         </div>
         <div class="col-md-4 offset-md-4 mt-1 pt-3">
-            <form action="/search" method="get">
+            <form action="/" method="get">
+                {{ csrf_field() }}
             <div class="input-group mb-3">
-                <input type="text" class="form-control" placeholder="Search ......" aria-label="Recipient's username" name="search">
+                <input type="text" class="form-control" placeholder="Search ......" aria-label="Recipient's username" name="search" value="{{ request('search') }}">
                 <button type="submit" class="btn btn-primary">Search</button>
             </div>
         </form>
         </div>
 
-<div class="row">
-    <div class="col-md-6">
-
-    </div>
-</div>
+        <div class="row">
+            @foreach ($search as $c)              
+            <div class="col-sm-6">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">{{ $c['name'] }}</h5>
+                  <p class="card-text">Location : {{ $c['location'] }}</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+            </div>
+            @endforeach
+          </div>
 
 
 
